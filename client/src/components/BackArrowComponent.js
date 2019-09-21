3aae48a1d358654230301a57e8a39e258787ca57
pakoraptor <xebrachu@gmail.com>
2019-09-21 18:15:59 +0700

    fixed Back Arrow link issue

--- client/src/components/BackArrowComponent.js
+++ client/src/components/BackArrowComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Link, Router, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { withRouter } from 'react-router';
 
 class BackArrow extends React.Component {
